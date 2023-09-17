f19184dfc3ffc2e6d1a706552c9ea87dd3632731
Bui Tuan Son <SonBTHE172207@fpt.edu.vn>
2023-09-18 05:45:06 +0700

    upload the core of project - spring boot 3.1

---
+++ src/main/java/com/kas/online_book_shop/OnlineBookShopApplication.java
@@ -0,0 +1,13 @@
+package com.kas.online_book_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kas/online_book_shop/OnlineBookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kas.online_book_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
