621fe7de7e57ce8cdf9ef644bb48d2fa97d6c67e
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-09-19 13:45:50 +0700

    add template

---
+++ src/main/java/com/kas/online_book_shop/controller/adminController.java
@@ -0,0 +1,14 @@
+package com.kas.online_book_shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/admin")
+@AllArgsConstructor
+
+public class adminController {
+    
+}
