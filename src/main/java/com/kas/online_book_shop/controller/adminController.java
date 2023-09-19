1123c0953543cbf6de75ee79c829f38a58bf7240
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-09-19 14:21:37 +0700

    Update adminController.java

--- src/main/java/com/kas/online_book_shop/controller/adminController.java
+++ src/main/java/com/kas/online_book_shop/controller/adminController.java
@@ -1,14 +1,15 @@
 package com.kas.online_book_shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.AllArgsConstructor;
 
 @Controller
 @RequestMapping("/admin")
-@AllArgsConstructor
 
-public class adminController {
-    
+public class AdminController {
+    @GetMapping("/home")
+    public String getHome(){
+        return "/admin/index.html";
+    }
 }
