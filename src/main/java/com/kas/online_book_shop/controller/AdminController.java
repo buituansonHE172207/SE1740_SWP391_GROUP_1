f13a9622446b34e5c7879db99e3c9d8a9eb97bf9
tattu2705 <tattupro2705@gmail.com>
2023-09-19 13:47:48 +0700

    template

---
+++ src/main/java/com/kas/online_book_shop/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.kas.online_book_shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/home")
+    public String getHome(){
+        return "admin/index";
+    }
+}
