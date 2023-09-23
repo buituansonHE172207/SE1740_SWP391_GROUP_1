d0ceadd61ba0ef118c52ccaa11629b7f546dc0d4
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-09-23 21:47:00 +0700

    update api

--- Backend/src/main/java/com/kas/online_book_shop/OnlineBookShopApplication.java
+++ Backend/src/main/java/com/kas/online_book_shop/OnlineBookShopApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookShopApplication {
-
+	/**
+	 * This is the main class!
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineBookShopApplication.class, args);
 	}
