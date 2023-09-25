3e18185393319d84d888a9b45f06426f4fc99add
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-09-25 16:36:13 +0700

    getUserByRoleId

--- Backend/src/main/java/com/kas/online_book_shop/controller/UserController.java
+++ Backend/src/main/java/com/kas/online_book_shop/controller/UserController.java
@@ -4,7 +4,9 @@ import java.util.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kas.online_book_shop.model.User;
@@ -26,5 +28,27 @@ public class UserController {
             return ResponseEntity.noContent().build();
         else
             return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/role/{roleId}")
+    public ResponseEntity<List<User>> getUserByRoleId(@PathVariable Long roleId) {
+        var users = userService.getUserByRoleId(roleId);
+
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(users);
+        }
+
     }
 }
--- Backend/src/main/java/com/kas/online_book_shop/service/UserService.java
+++ Backend/src/main/java/com/kas/online_book_shop/service/UserService.java
@@ -2,8 +2,15 @@ package com.kas.online_book_shop.service;
 
 import java.util.*;
 
+import com.kas.online_book_shop.model.Role;
 import com.kas.online_book_shop.model.User;
 
 public interface UserService {
     List<User> getAllUsers();
+
+    User getUserById(Long id);
+
+    List<User> getUserByRoleId(Long roleId);
+
+    // User getUserByEmail(String email);
 }
