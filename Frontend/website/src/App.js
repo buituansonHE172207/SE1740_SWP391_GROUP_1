4ebb50904166eef37de450440b4ed690f0d35b21
tattu2705 <tattupro2705@gmail.com>
2023-09-25 20:10:06 +0700

    Them url Collection

--- Frontend/website/src/App.js
+++ Frontend/website/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Header from './components/Header';
 import Home from './components/Home/Home';
+import Collection from './components/Collection/Collection';
 function App() {
   return (
     <div>
-      {/* <Router>
-          <Routes>
-            <Route path='/' Component={Slider}></Route>
-            
-          </Routes>
-        </Router> */}
         <Header/>
-        <Home/>
+
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/collections' Component={Collection} element={<Collection/>}></Route>
+        </Routes>
+
+      
     </div>
   );
 }
---
+++ Frontend/website/src/components/Collection/CollectionList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const CollectionList = (props) => {
+    return (
+        <div id="PageContainer">
+                <section id="collection-wrapper">
+                    <div className="container">
+                        <div className="row">
+                            {
+                                props.collections.map(collection => 
+                                    <div key={collection.id} className="col-lg-6">
+                                        <div className="collection-item-tacgia">
+                                            <div className="row">
+                                                <div className="col-lg-7">
+                                                    <Link to={`${collection.id}`}><img src="./img/book_shell.png"/></Link>
+                                                </div>
+                                                <div className="col-lg-5">
+                                                    <div className="c-author-listing--details">
+                                                        <div className="c-author-listing--title">
+                                                            <span className="word2 lastname">
+                                                                {collection.name}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>    
+                                )
+                            }
+                        </div>
+                    </div>
+                </section>
+        </div>
+    )
+}
+
+export default CollectionList
