f5483f6590fc9ec881ed98f924ce9ce7a44da87a
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-10-10 22:44:54 +0700

    update cors

--- Backend/src/main/java/com/kas/online_book_shop/controller/PublisherController.java
+++ Backend/src/main/java/com/kas/online_book_shop/controller/PublisherController.java
@@ -20,7 +20,8 @@ import com.kas.online_book_shop.service.PublisherService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost"})
+//@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost", "http://74.235.148.227:3000", "http://74.235.148.227:3001"})
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/publisher")
 public class PublisherController {
