6a2c3e3ec07a44c5b84a8493a04a5151d3b367db
Le Gia Nguyen <nguyenlghe176558@fpt.edu.vn>
2023-10-13 00:10:22 +0700

    https

--- Frontend/admin/src/http/index.tsx
+++ Frontend/admin/src/http/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://backend.sachtructuyen.shop/api/v1/",
+  baseURL: "https://backend.sachtructuyen.shop/api/v1/",
 });
 
 export const TOKEN = "token";
