2998f95b23901222059e3643af544a213d6f0098
buituansonHE172207 <SonBTHE172207@fpt.edu.vn>
2023-10-13 21:04:44 +0700

    update post api

--- Backend/src/main/java/com/kas/online_book_shop/controller/PostController.java
+++ Backend/src/main/java/com/kas/online_book_shop/controller/PostController.java
@@ -1,7 +1,5 @@
 package com.kas.online_book_shop.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kas.online_book_shop.enums.PostState;
 import com.kas.online_book_shop.model.Post;
 import com.kas.online_book_shop.model.PostCategory;
 import com.kas.online_book_shop.service.PostService;
@@ -32,23 +31,9 @@ import lombok.RequiredArgsConstructor;
 public class PostController {
     private final PostService postService;
 
-    @GetMapping("")
-    public ResponseEntity<List<Post>> getAllBooks() {
-        List<Post> posts = postService.getAllPosts();
-        if (posts.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        } else
-            return ResponseEntity.ok(posts);
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id) {
-        Post post = postService.getPostById(id);
-        if (post != null) {
-            return ResponseEntity.ok(post);
-        } else {
-            return ResponseEntity.noContent().build();
-        }
+        return ResponseEntity.ok(postService.getPostById(id));
     }
 
     @PostMapping
@@ -62,27 +47,30 @@ public class PostController {
         return ResponseEntity.ok(postService.updatePost(post));
     }
 
-    @GetMapping("/sorted-and-paged")
-    public Page<Post> getAllPostsSortedAndPaged(
+    @GetMapping("")
+    public ResponseEntity<Page<Post>> getAllPosts(
+            @RequestParam(required = false) PostCategory category,
+            @RequestParam(required = false) PostState state,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size,
             @RequestParam(defaultValue = "asc") String sortOrder) {
         Sort.Direction direction = (sortOrder.equalsIgnoreCase("asc")) ? Direction.ASC : Direction.DESC;
         Pageable pageable = PageRequest.of(page, size, direction, sortBy);
-        return postService.getAllPosts(pageable);
+        return ResponseEntity.ok(postService.getAllPostsWithSorterAndFilter(category, state, pageable));
     }
 
-    @GetMapping("/sorted-and-paged/by-categories")
-    public Page<Post> getPostsByCategoriesAndPriceBetween(
-            @RequestParam PostCategory category,
+    @GetMapping("/search")
+    public ResponseEntity<Page<Post>> searchPost(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) PostCategory category,
+            @RequestParam(required = false) PostState state,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size,
             @RequestParam(defaultValue = "asc") String sortOrder) {
         Sort.Direction direction = (sortOrder.equalsIgnoreCase("asc")) ? Direction.ASC : Direction.DESC;
         Pageable pageable = PageRequest.of(page, size, direction, sortBy);
-        return postService.getPostsByCategory(category, pageable);
+        return ResponseEntity.ok(postService.searchPostByTitleWithSorterAndFilter(title, category, state, pageable));
     }
-
 }
