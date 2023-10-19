f7d99114ce964a72f7b9d64cc88a45b75af95edb
buituansonHE172207 <SonBTHE172207@fpt.edu.vn>
2023-10-19 23:19:43 +0700

    fix cors

--- Backend/src/main/java/com/kas/online_book_shop/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/kas/online_book_shop/config/SecurityConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +20,21 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfiguration {
         private final JwtAuthenticationFilter jwtAuthenticationFilter;
         private final AuthenticationProvider authenticationProvider;
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.addAllowedOrigin("http://localhost:3000");
+                configuration.addAllowedOrigin("http://localhost:3001"); 
+                configuration.addAllowedOrigin("http://localhost"); 
+                configuration.addAllowedMethod("GET, POST, PUT, DELETE, OPTIONS");
+                configuration.addAllowedHeader("*"); // Chỉ định các tiêu đề cho phép.
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+
+                return source;
+        }
 
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
